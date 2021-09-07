06212847f0603dccf78552a5b6f0d0e0b82aa276
rumgalxdead <rumgal@gmail.com>
2021-09-07 23:30:35 +0200

    configuration du webpack

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
-btn.onclick = () => {
-    fetch("http://localhost:3000/api/teddies" + products.value)
+// init
+import retrieveContent from "./query.js";
 
-    .then(function(res) {
-            if (res.ok) {
-                return res.json()
-            }
-        })
-        // .then(function(products) {
-        //     console.table(res);
-        // })
-        .then(function(res) {
-            // products.textContent = `nos produits ${ res }`;
-            products.appendChild(res);
-        })
+async function showContent() {
+	try {
+		const content = await retrieveContent();
 
+		let elt = document.createElement("div");
+		elt.innerHTML = content.join("<br />");
+
+		document.getElementsByTagName("body")[0].appendChild(elt);
+	} catch (e) {
+		console.log("Error", e);
+	}
 }
+
+showContent();
+
+// init-end
+btn.onclick = () => {
+	fetch("http://localhost:3000/api/teddies" + products.value)
+		.then(function (res) {
+			if (res.ok) {
+				return res.json();
+			}
+		})
+		// .then(function(products) {
+		//     console.table(res);
+		// })
+		.then(function (res) {
+			// products.textContent = `nos produits ${ res }`;
+			products.appendChild(res);
+		});
+};
