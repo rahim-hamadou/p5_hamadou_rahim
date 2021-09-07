59e301ac2768a29fcb19c372746317cbc4754962
rumgalxdead <rumgal@gmail.com>
2021-09-08 01:32:10 +0200

    test webpack ok
    voir pour le server live port 8080 nok

--- app.js
+++ app.js
@@ -17,22 +17,25 @@ async function showContent() {
 showContent();
 
 // init-end
-btn.onclick = () => {
-	fetch("http://localhost:3000/api/teddies" + products.value)
-		.then(function (res) {
-			if (res.ok) {
-				return res.json();
-			}
-		})
-		// .then(function(products) {
-		//     console.table(res);
-		// })
-		.then(function (res) {
-			// products.textContent = `nos produits ${ res }`;
-			products.appendChild(res);
-		});
-};
+//p5
 
+// btn.onclick = () => {
+// 	fetch("http://localhost:3000/api/teddies" + products.value)
+// 		.then(function (res) {
+// 			if (res.ok) {
+// 				return res.json();
+// 			}
+// 		})
+// 		// .then(function(products) {
+// 		//     console.table(res);
+// 		// })
+// 		.then(function (res) {
+// 			// products.textContent = `nos produits ${ res }`;
+// 			products.appendChild(res);
+// 		});
+// };
+
+//p5-end
 // test js
 const footer = document.querySelector("footer");
-footer.style.background = "yellow";
+footer.style.background = "violet";
