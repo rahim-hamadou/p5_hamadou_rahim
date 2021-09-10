cd2e0e3b0a46b9fa8a1a7c870517459895272c31
rumgalxdead <rumgal@gmail.com>
2021-09-10 16:49:38 +0200

    page index ok

--- src/index.js
+++ src/index.js
@@ -1,35 +1,76 @@
 import retrieveContent from "./query.js";
 
-async function showContent() {
-	try {
-		const content = await retrieveContent();
-		console.log(content);
+// async function showContent() {
+// 	try {
+// 		const content = await retrieveContent();
+// 		console.log(content);
 
-		let dataHtml = "";
-		content.forEach((peluche) => {
-			dataHtml += "<p>Name : " + peluche["name"] + "</p>";
-			dataHtml += '<img src="' + peluche["imageUrl"] + '" alt="" id="articleImg">';
-			dataHtml += "<p>price : " + peluche["price"] + "</p>";
-		});
+// 		let dataHtml = "";
+// 		content.forEach((peluche) => {
+// 			dataHtml += "<p>Name : " + peluche["name"] + "</p>";
+// 			dataHtml += '<img src="' + peluche["imageUrl"] + '" alt="" id="articleImg">';
+// 			dataHtml += "<p>price : " + peluche["price"] + "</p>";
+// 		});
 
-		let peluches = document.getElementById("peluches");
-		peluches.innerHTML = dataHtml;
-		// elt.innerHTML = "<p>test</p>" + content.join('<br />');
+// 		let peluches = document.getElementById("peluches");
+// 		peluches.innerHTML = dataHtml;
+// 	} catch (e) {
+// 		console.log("Error", e);
+// 	}
+// }
 
-		// if ("content" in document.createElement("template")) {
+// showContent();
 
-		//   // On prépare une ligne pour le tableau
-		//   var template = document.querySelector("#productrow");
-		// }
+// ----------------------------
+const liste = document.querySelector(".liste");
+const btn = document.querySelector(".btn");
+//on lie les elements HTML a des variables JS
 
-		// document.getElementsByTagName('body')[0].appendChild(elt);
-	} catch (e) {
-		console.log("Error", e);
-	}
-}
+// btn.addEventListener("click", () => {
+fetch("http://localhost:3000/api/teddies")
+	// appel a l'API via fetch
+	.then((reponse) => reponse.json())
+	//ensuite transformation de la promesse en json
+	// .then((data) => console.log(data))
+	//affichage du contenu des data de la reponse.json
+	.then((data) => {
+		//creation de la boucle qui va permettre d'ajouter dynamiquement du contenu
+		for (let i = 0; i < data.length; i++) {
+			//on itere dans les elements de response
 
-showContent();
+			let newLi = document.createElement("li");
+			let newTitreCarte = document.createElement("h2");
+			let newBodyCarte = document.createElement("p");
+			let newPrice = document.createElement("p");
+			let newImg = document.createElement("img");
+			let newBtn = document.createElement("button");
+			//on cree les elements qui vont servir de cible.
 
-// test js
-const footer = document.querySelector("footer");
-footer.style.background = "violet";
+			newBtn.innerHTML = "Commander";
+			newBtn.value = "./produit.html";
+			newTitreCarte.innerText = data[i].name;
+			// newTitreCarte.innerText = data[i].title;
+			newBodyCarte.innerText = data[i].description;
+			// newBodyCarte.innerText = data[i].body;
+			newPrice.innerText = data[i].price;
+			//on apporte du contenu aux elements cree par le biais de chaque element [i] de data.
+
+			newImg.setAttribute("src", data[i].imageUrl);
+			newImg.setAttribute("alt", "image de peluche");
+
+			newLi.appendChild(newImg);
+			newLi.appendChild(newTitreCarte);
+			newLi.appendChild(newBodyCarte);
+			newLi.appendChild(newPrice);
+			newLi.appendChild(newBtn);
+			//on ajoute donc les elements a la cible (les li cree dynamiquement)
+
+			liste.appendChild(newLi);
+			//enfin on ajoutes ces li au ul principale
+
+			newBtn.addEventListener("click", function () {
+				window.location = "../produit.html";
+				//creation de la fonction qui redirige vers la page produits
+			});
+		}
+	});
