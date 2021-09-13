420bdfd382cf772289946d3f29d09bb06d1dbcbd
rumgalxdead <rumgal@gmail.com>
2021-09-13 13:25:53 +0200

    ajout d uproduit sur la page produit ok
    configuration du choix des couleurs via un input en cours

--- produit.js
+++ produit.js
@@ -1,3 +1,111 @@
-const test = document.querySelector(".shopping-product");
+//-------------------- Test page produit.js
+const test = document.querySelector(".main-title-choice");
 
-test.style.background = "red";
+test.style.background = "violet";
+
+// -------------------recuperation de l'id url par produit
+
+const queryString_url_id = window.location.search;
+// on recupupere le complement de l'url
+
+// //  -------------------------test 1:
+// const idPage_produit = queryString_url_id.slice(4);
+// // recuperation de l'id dans l'url du produit dans le complement url, en coupant la valeur recuperé d uwindow.location.search
+// console.log(queryString_url_id);
+// console.log(idPage_produit);
+
+//  -------------------------test 2:-------------
+
+const urlSearchParams = new URLSearchParams(queryString_url_id);
+// application du constructor URLSearchParams
+// console.log(urlSearchParams);
+
+const id = urlSearchParams.get("id");
+// recuperation de l'id via la methode get du constructor (recupere l'element id= dans l'url)
+// console.log(id);
+//on verifie l'id par parge produit
+
+// ------------------------affichage en fonction de l'id-----------
+
+//----------faire un fetch avec un slash suivi de l'id en fin d'url-------
+
+let ficheProduit = fetch("http://localhost:3000/api/teddies/" + id)
+	.then((reponse) => reponse.json())
+	// .then((dataProduit) => {
+	// 	console.log(dataProduit);
+	// 	// on verifie ce que nous retourne le fetch
+	// })
+	.then((dataProduit) => {
+		// ---------Preparer la page a recevoir le contenu du produit
+		const shoppingProduit = document.querySelector(".shopping-product");
+		// console.log(dataProduit);
+		const listeProduits = document.querySelector(".listeProduits");
+		console.log(listeProduits);
+		const btnProduit = document.querySelector(".btn");
+		// Selection de la cible du html dynamique
+
+		// liaison de l'image du produit via le data de la response fetch
+
+		let liProduit = document.createElement("li");
+		let titreCarteProduit = document.createElement("h2");
+		let titreBodyProduit = document.createElement("p");
+		let priceProduit = document.createElement("p");
+		//on cree les element html
+		let imgProduit = document.createElement("img");
+		imgProduit.setAttribute("src", dataProduit.imageUrl);
+		imgProduit.setAttribute("alt", "image de peluche");
+		//on cree l'image
+		let btnValideProduit = document.createElement("button");
+		//creation du bouton
+		let formProduit = document.createElement("form");
+		//creation d'un formulaire
+		let optionProduit = document.createElement("input");
+		optionProduit.type = "radio";
+		optionProduit.for = "optionProduit";
+		optionProduit.label = "choixCouleur";
+		optionProduit.value = dataProduit.colors;
+		let labelCouleur = document.createElement("label");
+		labelCouleur.htmlFor = "couleurs";
+
+		// On crée un form
+		console.log(optionProduit);
+		optionProduit.action = "/cgi-bin/some.cgi";
+		// On y ajoute des attributs action et method
+		optionProduit.method = "POST";
+		// optionProduit.submit();
+		// On appelle la méhtode submit pour l'envoyer
+
+		//on cree les elements qui vont servir de cible.
+		console.log(btnValideProduit);
+
+		btnValideProduit.innerHTML = "Ajouter au panier";
+		btnValideProduit.type = "submit";
+		btnValideProduit.name = "btnValideProduit";
+		btnValideProduit.value = "./panier.html";
+		//creation du bouton panier et de sa destination
+
+		titreCarteProduit.innerText = dataProduit.name;
+		// newTitreCarte.innerText = data[i].title;
+		titreBodyProduit.innerText = dataProduit.description;
+		// newBodyCarte.innerText = data[i].body;
+		priceProduit.innerText = dataProduit.price / 100 + " €";
+		//on apporte du contenu aux elements cree par le biais de chaque element [i] de data.
+
+		liProduit.appendChild(imgProduit);
+		liProduit.appendChild(titreCarteProduit);
+		liProduit.appendChild(titreBodyProduit);
+		liProduit.appendChild(priceProduit);
+
+		formProduit.appendChild(optionProduit);
+		liProduit.appendChild(labelCouleur);
+		console.log(labelCouleur);
+		liProduit.appendChild(formProduit);
+		liProduit.appendChild(btnValideProduit);
+		// On l'ajoute au corps du document
+
+		//on ajoute donc les elements a la cible (les li cree dynamiquement)
+		console.log(listeProduits);
+
+		listeProduits.appendChild(liProduit);
+		// //enfin on ajoutes ces li au ul principale
+	});
--- src/index.js
+++ src/index.js
@@ -28,68 +28,95 @@ const mainTitle = document.querySelector(".main-title");
 //on lie les elements HTML a des variables JS
 console.log(mainTitle);
 
-btn.addEventListener("click", () => {
-	mainTitle.textContent = "Ils sont trop mignons";
+// btn.addEventListener("click", () => {
+// 	mainTitle.textContent = "Ils sont trop mignons";
 
-	fetch("http://localhost:3000/api/teddies")
-		// appel a l'API via fetch
-		.then((reponse) => reponse.json())
-		//ensuite transformation de la promesse en json
-		// .then((data) => console.log(data))
-		//affichage du contenu des data de la reponse.json
-		.then((data) => {
-			//creation de la boucle qui va permettre d'ajouter dynamiquement du contenu
-			for (let i = 0; i < data.length; i++) {
-				//on itere dans les elements de response
+fetch("http://localhost:3000/api/teddies")
+	// appel a l'API via fetch
+	.then((reponse) => reponse.json())
+	//ensuite transformation de la promesse en json
+	// .then((data) => console.log(data))
+	//affichage du contenu des data de la reponse.json
+	.then((data) => {
+		//creation de la boucle qui va permettre d'ajouter dynamiquement du contenu
+		for (let i = 0; i < data.length; i++) {
+			//on itere dans les elements de response
 
-				const products = [data[i]];
-				//creation d'un tableau avec les differents produits
-				// console.log(products);
+			const products = [data[i]];
+			//creation d'un tableau avec les differents produits
+			// console.log(products);
 
-				const newIdCarteUrl = data[i]._id;
-				//creation de la variable url lié a l'id
-				// console.log(newIdCarteUrl);
+			const newIdCarteUrl = data[i]._id;
+			//creation de la variable url lié a l'id
+			// console.log(newIdCarteUrl);
 
-				let newLi = document.createElement("li");
-				let newTitreCarte = document.createElement("h2");
-				let newBodyCarte = document.createElement("p");
-				let newPrice = document.createElement("p");
-				let newImg = document.createElement("img");
-				let newBtn = document.createElement("button");
-				//on cree les elements qui vont servir de cible.
+			const nomProduit = data[i].name;
+			const descriptionProduit = data[i].description;
+			const priceProduit = data[i].price;
+			const imgUrlProduit = data[i].imageUrl;
+			console.log(imgUrlProduit);
+			// declaration des variables
 
-				newBtn.innerHTML = "Commander";
-				newBtn.value = "./produit.html";
-				//creation du bouton et de sa destination
+			let newLi = document.createElement("li");
+			let newTitreCarte = document.createElement("h2");
+			let newBodyCarte = document.createElement("p");
+			let newPrice = document.createElement("p");
+			let newImg = document.createElement("img");
+			let newBtn = document.createElement("button");
+			//on cree les elements qui vont servir de cible.
 
-				newTitreCarte.innerText = data[i].name;
-				// newTitreCarte.innerText = data[i].title;
-				newBodyCarte.innerText = data[i].description;
-				// newBodyCarte.innerText = data[i].body;
-				newPrice.innerText = data[i].price / 100 + " €";
-				//on apporte du contenu aux elements cree par le biais de chaque element [i] de data.
+			newBtn.innerHTML = "Commander";
+			newBtn.value = "./produit.html";
+			//creation du bouton et de sa destination
 
-				newImg.setAttribute("src", data[i].imageUrl);
-				newImg.setAttribute("alt", "image de peluche");
-				// liaison de l'image du produit via le data de la response fetch
+			newTitreCarte.innerText = data[i].name;
+			// newTitreCarte.innerText = data[i].title;
+			newBodyCarte.innerText = data[i].description;
+			// newBodyCarte.innerText = data[i].body;
+			newPrice.innerText = data[i].price / 100 + " €";
+			//on apporte du contenu aux elements cree par le biais de chaque element [i] de data.
 
-				newLi.appendChild(newImg);
-				newLi.appendChild(newTitreCarte);
-				newLi.appendChild(newBodyCarte);
-				newLi.appendChild(newPrice);
-				newLi.appendChild(newBtn);
-				//on ajoute donc les elements a la cible (les li cree dynamiquement)
+			newImg.setAttribute("src", data[i].imageUrl);
+			newImg.setAttribute("alt", "image de peluche");
+			// liaison de l'image du produit via le data de la response fetch
 
-				liste.appendChild(newLi);
-				//enfin on ajoutes ces li au ul principale
+			newLi.appendChild(newImg);
+			newLi.appendChild(newTitreCarte);
+			newLi.appendChild(newBodyCarte);
+			newLi.appendChild(newPrice);
+			newLi.appendChild(newBtn);
+			//on ajoute donc les elements a la cible (les li cree dynamiquement)
 
-				newBtn.addEventListener("click", function () {
-					window.location = "../produit.html?" + newIdCarteUrl;
-					//creation de la fonction qui redirige vers la page produit selon l'id
-					//cela grace au ? et a la variable recuperant l'id
-				});
-			}
+			liste.appendChild(newLi);
+			//enfin on ajoutes ces li au ul principale
 
-			//-----------------------------------page produit
-		});
-});
+			// // 					test implementation html
+
+			// let structureHtml = "";
+			// structureHtml += `<li>
+			// 		<img src=${imgUrlProduit} alt="image de peluche" />
+			// 		<h2>${nomProduit}</h2>
+			// 			<p>
+			// 				${descriptionProduit}
+			// 			</p>
+			// 			<p>${priceProduit} €</p>
+			// 		<button value="./produit.html">Commander</button>
+			// </li>`;
+
+			// //							injection HTML
+
+			// //
+			// liste.innerHTML = structureHtml;
+
+			// // fin  implementation html
+
+			newBtn.addEventListener("click", function () {
+				window.location = "../produit.html?" + "id=" + newIdCarteUrl;
+				//creation de la fonction qui redirige vers la page produit selon l'id
+				//cela grace au ? et a la variable recuperant l'id
+			});
+		}
+
+		//-----------------------------------page produit
+	});
+// });
